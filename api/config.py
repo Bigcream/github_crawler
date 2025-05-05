c36716d6e1cefd4bbbf8f8e33c75995a9764a4f4
bigcream <duyk63uet@gmail.com>
2025-05-05 21:51:23 +0700

    update crawler use scrapy redis

--- api/config.py
+++ api/config.py
@@ -1,6 +1,9 @@
 class Config:
-    DB_HOST = "localhost"
+    DB_HOST = "postgres"  # Sử dụng tên dịch vụ PostgreSQL
     DB_NAME = "github_crawler"
     DB_USER = "crawler"
     DB_PASSWORD = "crawler"
     DB_PORT = "5432"
+    REDIS_HOST = "redis"  # Hoặc IP của Redis server
+    REDIS_PORT = 6379
+    REDIS_DB = 0
